0e887581d4890a2e21ff25c7a7fb9e8797554d34
nclevine <nclevine@gmail.com>
2024-08-27 15:10:08 -0400

    Updates to navbar

--- app/components/Navigation/Navigation.tsx
+++ app/components/Navigation/Navigation.tsx
@@ -1,27 +1,51 @@
 "use client"
 
+import { useState } from 'react';
 import Link from 'next/link';
-import CabinLogo from '../SVG/cabinLogo.svg'
+import styles from './Navigation.module.css';
+import MobileLogo from '../SVG/mobileLogo.svg';
+import DesktopLogo from '../SVG/desktopLogo.svg';
 
 export default function Navigation() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const closeMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <nav>
-      <ul>
-        <li>
-          <Link href="/">
-            <CabinLogo />
+    <nav className={styles.navbar}>
+      <div className={styles.navContent}>
+        <div className={styles.logoWrapper}>
+          <Link href="/" legacyBehavior>
+            <a className={styles.logoLinkWrapper}>
+              <div className={styles.mobileLogoWrapper}>
+                <MobileLogo className={styles.mobileLogo} />
+              </div>
+              <div className={styles.desktopLogoWrapper}>
+                <DesktopLogo className={styles.desktopLogo} />
+              </div>
+            </a>
           </Link>
-        </li>
-        <li>
-          <Link href="/menu">Menu</Link>
-        </li>
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/reservations">Reservations</Link>
-        </li>
-      </ul>
+        </div>
+
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <span className={`${styles.line} ${isMenuOpen ? styles.lineTop : ''}`}></span>
+          <span className={`${styles.line} ${isMenuOpen ? styles.lineBottom : ''}`}></span>
+        </div>
+
+        <div className={`${styles.navLinksContainer} ${isMenuOpen ? styles.menuOpen : ''}`}>
+          <ul className={styles.navLinks}>
+            <li onClick={closeMenu}><Link href="/menu">Menu</Link></li>
+            <li onClick={closeMenu}><Link href="/reservations">Reservations</Link></li>
+            <li onClick={closeMenu}><Link href="/about">About</Link></li>
+          </ul>
+        </div>
+      </div>
     </nav>
   );
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navigation />
-        {children}
+        <main>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
