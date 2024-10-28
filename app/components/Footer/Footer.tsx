e5493d8925c35bfd14b621e5aefc2130aa76c4f2
nclevine <nclevine@gmail.com>
2024-10-28 16:48:47 -0400

    Added mail button

--- app/components/Footer/Footer.tsx
+++ app/components/Footer/Footer.tsx
@@ -1,5 +1,6 @@
 import styles from './Footer.module.css';
 import Instagram from '../SVG/instagram.svg';
+import Mail from '../SVG/mail.svg';
 
 type FooterProps = {
   address: string;
@@ -15,10 +16,17 @@ export default function Footer({ address, phoneNumber }: FooterProps) {
           <a href={`tel:+1${phoneNumber.replace(/[^0-9]/g, '')}`}>{phoneNumber}</a>
         </p>
       </div>
-      <div className={styles.instagram}>
-        <a href="https://www.instagram.com/coetavernjh/" target="_blank" rel="noopener noreferrer">
-          <Instagram className={styles.instagramLogo} />
-        </a>
+      <div className={styles.logos}>
+        <div className={styles.instagram}>
+          <a href="https://www.instagram.com/coetavernjh/" target="_blank" rel="noopener noreferrer">
+            <Instagram className={styles.instagramLogo} />
+          </a>
+        </div>
+        <div className={styles.instagram}>
+          <a href="mailto:info@coetavern.com" target="_blank" rel="noopener noreferrer">
+            <Mail className={styles.instagramLogo} />
+          </a>
+        </div>
       </div>
     </footer>
   );
